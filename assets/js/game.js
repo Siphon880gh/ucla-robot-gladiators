30f1effe87ee7fd1229b5ed6c671ae1102bc3473
Weng Fei Fung <weffung@ucdavis.edu>
2020-10-19 01:02:20 -0700

    Fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
 console.info("%cApp init: %cPassed", "font-weight:bold", "font-style:italic; color:green;");
+
+// Robot fights
+function fight() {
+    console.info("The fight has begun!");
+}
+
+fight();
