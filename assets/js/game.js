5ca7890351b497b6a19596dee35dbb611e15b487
Weng Fei Fung <weffung@ucdavis.edu>
2020-10-19 01:56:39 -0700

    One round fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,9 +6,40 @@ class Robot {
     }
 } // Robot
 
-// Robot fights
-function fight() {
-    console.info("The fight has begun!");
+/** <<< Code above are notes */
+
+var playerName = "";
+while(playerName==="" || playerName===null) {
+    playerName = window.prompt("What is your robot's name?");
 }
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this
+console.log("Debug: Your stats:")
+console.table({playerName, playerAttack, playerHealth});
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var firstFight = true;
+
+var fight = function() {
+    if(firstFight) {
+        firstFight = false;
+        window.alert("Welcome to Robot Gladiators!");
+    }
+
+    const oriPlayerHealth = playerHealth;
+    const oriEnemyHealth = enemyHealth;
+
+    playerHealth -= enemyAttack;
+    enemyHealth -= playerAttack;
+
+    console.log(`${playerName} attacks ${enemyName}... enemy health points decreased ${oriEnemyHealth} --> ${enemyHealth}`);
+    console.log(`${enemyName} attacks ${playerName}... your health points decreased ${oriPlayerHealth} --> ${playerHealth}`);
+
+};
 
 fight();
